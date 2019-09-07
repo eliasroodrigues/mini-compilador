44fa103c9ca245ff019e09b9e08cd526ceb3a28d
Elias Rodrigues <elias.roodrigues98@gmail.com>
2019-09-07 20:09:56 -0300

    New files for inter package

---
+++ inter/Temp.java
@@ -0,0 +1,16 @@
+package inter;
+
+import lexer.*; import symbols.*;
+
+/*
+* Os nomes temporários têm tipo como todas as outras
+* expressões. O construtor Temp. portanto, é chamado com
+* um tipo como parâmetro.
+*/
+
+public class Temp extends Expr {
+	static int count = 0;
+	int number = 0;
+	public Temp(Type t) { super(Word.temp, p); number = ++count; }
+	public String toString() { return "t" + number; } 
+}
---
+++ inter/Unary.java
@@ -0,0 +1,19 @@
+package inter;
+
+import lexer.*; import symbols.*;
+
+/*
+* A classe Unary é o correspondente de um operador da classe Arith.
+*/
+
+public class Unary extends Op {
+	public Expr expr;
+	public Unary(Token tok, Expr x) { // trata operador menos, para !
+		super(tok, null); expr = x;
+		type = Type.max(Type.Int, expr.type);
+		if (type == null) error("type error");
+	}
+
+	public Expr gen() { return new Unary(op, expr.reduce()); }
+	public Strin toString() { return op.toString() + " " + expr.toString(); }
+}
